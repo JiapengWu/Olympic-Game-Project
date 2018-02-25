94ef9adfd3b20a28ecae24e99c93b4d3a62568ec
JiapengWu <jiapeng.wu@mail.mcgill.ca>
2018-02-25 18:27:13 -0500

    modified participate

--- table_creation_scripts/participate.sql
+++ table_creation_scripts/participate.sql
@@ -5,8 +5,12 @@ CREATE TABLE cs421g19.participate
   ,result varchar(50)
   ,ranking INTEGER
   ,medal_type VARCHAR(50)
-  ,PRIMARY KEY( sname,team_type,gender)
+  ,PRIMARY KEY(player_id,match_id)
   ,foreign key(player_id) REFERENCES player(player_id)
   ,foreign key(match_id) REFERENCES match(match_id)
   ,foreign key(medal_type) REFERENCES medals(medal_type)
 );
+
+
+INSERT INTO participate VALUES(1, 1, '17.03 s', 3, null);
+INSERT INTO participate VALUES(1, 1, '19.03 s', 3, null;
