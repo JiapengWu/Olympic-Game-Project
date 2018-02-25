4ba440f61c9bbc7e58af39a3650f389623312e46
mathieuvachon2 <vmat96@gmail.com>
2018-02-25 16:49:52 -0500

    added tid and insert values now match with tid

--- table_creation_scripts/spectator.sql
+++ table_creation_scripts/spectator.sql
@@ -2,11 +2,11 @@
 CREATE TABLE cs421g19.spectator
 (
    sid INTEGER NOT null unique
+   tid integer 
   ,sname varchar(50)
-  ,PRIMARY KEY( sid)                                                                                                                                          
+  ,PRIMARY KEY( sid)
+  ,foreign key(tid) references ticket(tid)
 );
 
-INSERT INTO match VALUES(12, 'David Gilbert');
-INSERT INTO match VALUES(23, 'Mathieu Vachon');
-INSERT INTO match VALUES(34, 'Daniel Chernis');
-INSERT INTO match VALUES(46, 'Paul Wu');
+INSERT INTO match VALUES(12, 7, 'David Gilbert');
+INSERT INTO match VALUES(23, 42, 'Mathieu Vachon');
