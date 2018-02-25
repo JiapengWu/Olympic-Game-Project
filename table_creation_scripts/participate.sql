8f853c35b856be331e7540e2fdd536e239e946d4
mathieuvachon2 <vmat96@gmail.com>
2018-02-25 16:28:43 -0500

    participate changed by Matty V

--- table_creation_scripts/participate.sql
+++ table_creation_scripts/participate.sql
@@ -8,6 +8,7 @@ CREATE TABLE cs421g19.participate
   ,PRIMARY KEY( sname,team_type,gender)
   ,foreign key(player_id) REFERENCES player(player_id)
   ,foreign key(match_id) REFERENCES match(match_id)
+  ,foreign key(medal_type) REFERENCES medals(medal_type)
 );
 
 INSERT INTO category VALUES(1,1,'11sec', 1,'gold');
