c9f17dedea555bd877539a1f7577ecb2b229acc3
JiapengWu <jiapeng.wu@mail.mcgill.ca>
2018-02-25 13:37:43 -0500

    renamed scripts

---
+++ table_creation_scipts/spectator.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE cs421g19.spectator
+(
+   sid INTEGER NOT null unique
+  ,sname varchar(50)
+  ,PRIMARY KEY( sid)                                                                                                                                          
+);
+
+INSERT INTO match VALUES(12, 'David Gilbert');
+INSERT INTO match VALUES(23, 'Mathieu Vachon');
+INSERT INTO match VALUES(34, 'Daniel Chernis');
+INSERT INTO match VALUES(46, 'Paul Wu');
