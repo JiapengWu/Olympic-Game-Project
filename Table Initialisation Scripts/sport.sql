d512ccc7eadf39642421fc7b64bcc20398847990
Daniel Chernis <32421770+dch133@users.noreply.github.com>
2018-02-25 13:49:56 -0500

    moved queries from Scripts folder

---
+++ Table Initialisation Scripts/sport.sql
@@ -0,0 +1,14 @@
+
+
+CREATE TABLE cs421g19.sport
+(
+   sname VARCHAR(500) NOT null unique
+  ,distance INTEGER
+  ,PRIMARY KEY(sname)                                                                                                                                         
+);
+
+INSERT INTO Sport VALUES( 'Breaststroke', 200);
+INSERT INTO Sport VALUES( 'Butterfly', 100);
+INSERT INTO Sport VALUES( 'Freestyle', 400);
+INSERT INTO Sport VALUES( 'Sprint', 100);
+INSERT INTO Sport VALUES( 'Javelin Throw', null);
