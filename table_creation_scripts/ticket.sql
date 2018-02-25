bd841e4dc4953807d3a8ed205f6d6ecd586abc80
mathieuvachon2 <vmat96@gmail.com>
2018-02-25 16:41:41 -0500

    added foreign keys and not nulls

--- table_creation_scripts/ticket.sql
+++ table_creation_scripts/ticket.sql
@@ -2,10 +2,14 @@
 CREATE TABLE cs421g19.ticket
 (
    tid INTEGER NOT null unique
-  ,seat_number varchar(50)
-  ,price float
-  ,PRIMARY KEY( tid)                                                                                                                                         
+  ,match_id integer
+  ,sid integer 
+  ,seat_number varchar(50) not null
+  ,price float not null
+  ,PRIMARY KEY(tid)
+  ,foreign key(match_id) references match(match_id)
+  ,foreign key(sid) references spectator(sid)
 );
 
-INSERT INTO ticket VALUES(42, '55f', 50.99);
-INSERT INTO ticket VALUES(7, '244A', 249.99);
+INSERT INTO ticket VALUES(42, 1, 12, '55f', 50.99);
+INSERT INTO ticket VALUES(7, 2, 23, '244A', 249.99);
