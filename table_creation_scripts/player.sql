506ad2e296f6bb40fe60518ff7c1a6da1313d884
dch133 <daniel.chernis@mail.mcgill.ca>
2018-02-25 14:58:08 -0500

    added oreign key country name

--- table_creation_scripts/player.sql
+++ table_creation_scripts/player.sql
@@ -2,19 +2,18 @@
 CREATE TABLE cs421g19.player
 (
    player_id INTEGER NOT null unique
-  ,cname VARCHAR(50) NOT NULL
   ,pname VARCHAR(50) NOT NULL
-  ,gender VARCHAR(50)
+  ,gender VARCHAR(50) not null
+  ,cname varchar(50) 
   ,gold_number INTEGER
   ,silver_number INTEGER
   ,bronze_number INTEGER
   ,PRIMARY KEY(player_id)
-  ,foreign key(cname) REFERENCES country(cname)
-
+  ,FOREIGN KEY(cname) REFERENCES match(country)
 );
 
-INSERT INTO player VALUES(1, 'David Gilbert','female', 6, 6, 6);
-INSERT INTO player VALUES(5, 'Paul Wu', 'male', 2, 5, 6);
-INSERT INTO player VALUES(3, 'Daniel Chernis', 'male', 3, 5, 0);
-INSERT INTO player VALUES(7, 'Mathieu Vachon', 'male', 5, 8, 7);
-INSERT INTO player VALUES(9, 'Bruce Wayne','male', 3, 2, 1);
+INSERT INTO player VALUES(1, 'David Gilbert','female','Japan', 6, 6, 6);
+INSERT INTO player VALUES(5, 'Paul Wu', 'male','China' , 2, 5, 6);
+INSERT INTO player VALUES(3, 'Daniel Chernis', 'male','Canada' , 3, 5, 0);
+INSERT INTO player VALUES(7, 'Mathieu Vachon', 'male','France' , 5, 8, 7);
+INSERT INTO player VALUES(9, 'Bruce Wayne','male','USA' ,3, 2, 1);
