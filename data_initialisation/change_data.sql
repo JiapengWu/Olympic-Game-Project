71df87946e3cc0d47b61ceaa8c47c7d7db045203
david-gilbert-cs <david.gilbert@mail.mcgill.ca>
2018-02-26 16:04:12 -0500

    total medal per country

--- data_initialisation/change_data.sql
+++ data_initialisation/change_data.sql
@@ -4,3 +4,20 @@ Update country
 	(select sum(player.gold_number) from player
 	 where country.cname = player.cname
 	);
+
+
+select * 
+from country 
+order by total_medal_number,gold_number,silver_number,bronze_number  desc;
+
+
+update country
+	SET total_medal_number = 
+(
+SELECT sum(country.gold_number)+
+		sum(country.silver_number)+
+		sum(country.bronze_number) 
+FROM country where country.cname = 'France'
+)
+where country.cname = 'France'
+;
