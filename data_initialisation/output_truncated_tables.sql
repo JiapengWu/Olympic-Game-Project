d4e62f510c63307f2aee463e540c362b73eff878
dch133 <daniel.chernis@mail.mcgill.ca>
2018-02-28 15:51:59 -0500

    see truncated out put for each table (up to 8 tuples)

---
+++ data_initialisation/output_truncated_tables.sql
@@ -0,0 +1,10 @@
+--output each table truncated to first 8 tuples
+
+select * from sports limit 8;
+select * from player limit 8;
+select * from matches limit 8;
+select * from participate limit 8;
+select * from spectator limit 8;
+select * from ticket limit 8;
+select * from country limit 8;
+
